228da3009176f84ba64a85a806528fc87d0d4635
alcaldia05 <117576633+alcaldia05@users.noreply.github.com>
2022-12-03 19:03:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_id = 11029886
-api_hash = '4e74899bfd41879c6a4b48cf6a07f456'
-bot_token = ''
+api_id = 14681595
+api_hash = 'a86730aab5c59953c424abb4396d32d5'
+bot_token = '5759495332:AAGjdS7R0c3-VZFlhQbNJm4GKCSgdjEfQVo'
